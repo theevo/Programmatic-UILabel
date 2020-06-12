f1c0a420dca7a2cb3b07b54e8cbff49da34b9d65
Theo Vora <theo.vora@gmail.com>
2020-06-11 19:10:58 -0600

    Refactor

--- Programmatic Text Label/ViewController.swift
+++ Programmatic Text Label/ViewController.swift
@@ -10,24 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var label: UILabel!
+    let height: CGFloat = 35
+    let width: CGFloat = 105
+    var xOffset: CGFloat {
+        return -width / 2
+    }
+    var viewCenter: CGPoint = CGPoint(x: 100, y: 100)
+    var center: CGPoint {
+        get {
+            return CGPoint(x: self.viewCenter.x + xOffset, y: self.viewCenter.y)
+        }
+    }
+    var box: CGRect {
+        get {
+            return CGRect(origin: self.center, size: CGSize(width: self.width, height: self.height))
+        }
+    }
+    
+    var helloLabel: UILabel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let width: CGFloat = 105
-        let xOffset: CGFloat = -width / 2
-        let height: CGFloat = 35
+        viewCenter = view.center
         
-        var center = view.center
-        center.x = center.x + xOffset
+        buildLabel()
         
-        label = UILabel(frame: CGRect(origin: center, size: CGSize(width: width, height: height)))
-        label.font = UIFont(descriptor: UIFontDescriptor(name: "Courier", size: CGFloat(integerLiteral: 15)), size: CGFloat(integerLiteral: 15))
-        label.text = "Hello world"
-        label.layer.borderColor = UIColor.systemPink.cgColor
-        label.layer.borderWidth = 2.0
-        label.isUserInteractionEnabled = true
+        guard let label = helloLabel else { return }
+        
         view.addSubview(label)
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(buhBye))
@@ -36,8 +46,19 @@ class ViewController: UIViewController {
     }
     
     @objc func buhBye() {
+        guard let label = helloLabel else { return }
         label.removeFromSuperview()
-        label = nil
+        helloLabel = nil
+    }
+    
+    func buildLabel() {
+        let label = UILabel(frame: box)
+        label.font = UIFont(descriptor: UIFontDescriptor(name: "Courier", size: CGFloat(integerLiteral: 15)), size: CGFloat(integerLiteral: 15))
+        label.text = "Hello world"
+        label.layer.borderColor = UIColor.systemPink.cgColor
+        label.layer.borderWidth = 2.0
+        label.isUserInteractionEnabled = true
+        helloLabel = label
     }
 
 
